39d637cf6ce9e1d24d165518db1eb1941723338f
Bryce Matsuda <iexist4jcore@gmail.com>
2015-04-25 13:52:24 -0700

    Graph now draws appropriate graph when years are adjusted.

--- data-controller.js
+++ data-controller.js
@@ -13,6 +13,11 @@ var agop = angular.module('agop', []);
 // Data controller that defines button behavior
 agop.controller('ButtonController', ['$scope', function($scope) {
 
+  // At the beginning of execution,
+  // the default start and end year is 2005 and 2015, respectively.
+  $scope.startYear = 2005;
+  $scope.endYear = 2014;
+
     // Create the chart that will be used to animate year-by-year information.
     $scope.chart= new google.visualization.AreaChart(document.getElementById('visualization_div'));
 
@@ -21,44 +26,44 @@ agop.controller('ButtonController', ['$scope', function($scope) {
     // variable shouldn't be exposed to other controllers, so
     // I'm not adding it to the scope.
     var options = { "titleTextStyle": { color: "black",
-                    fontName: "Open Sans",
-                    fontSize: 16,
-                    bold: false,
-                    italic: false },
-            "title":"Video Game Releases Across Time by Platform",
-            "titleFontSize": 12,
-            "animation": {
-            startup: true,
-            duration: 850,
-            easing: 'in'
-            },
-            "bar": {"groupWidth": "95%"},
-            "vAxis": {"title": "Number of Releases",
-                  "viewWindow" : {max : 3000},
+    fontName: "Open Sans",
+    fontSize: 16,
+    bold: false,
+    italic: false },
+    "title":"Video Game Releases Across Time by Platform",
+    "titleFontSize": 12,
+    "animation": {
+      startup: true,
+      duration: 850,
+      easing: 'in'
+    },
+    "bar": {"groupWidth": "95%"},
+    "vAxis": {"title": "Number of Releases",
+                  // "viewWindow" : {max : 3000},
                   "textStyle" : { color: "black",
-                          fontName: "Open Sans",
-                          fontSize: 12,
-                          bold: false,
-                          italic: false },
+                  fontName: "Open Sans",
+                  fontSize: 12,
+                  bold: false,
+                  italic: false },
                   "titleTextStyle" : { color: "black",
-                           fontName: "Open Sans",
-                           fontSize: 14,
-                           bold: false,
-                           italic: false }},
-            "hAxis": {"title": "Years",
+                  fontName: "Open Sans",
+                  fontSize: 14,
+                  bold: false,
+                  italic: false }},
+                  "hAxis": {"title": "Years",
                   "format": '####',
                   "textStyle" : { color: "black",
-                          fontName: "Open Sans",
-                          fontSize: 12,
-                          bold: false,
-                          italic: false },
+                  fontName: "Open Sans",
+                  fontSize: 12,
+                  bold: false,
+                  italic: false },
                   "titleTextStyle" : { color: "black",
-                           fontName: "Open Sans",
-                           fontSize: 14,
-                           bold: false,
-                           italic: false }},
-            "legend": { "position": "right" }
-    };
+                  fontName: "Open Sans",
+                  fontSize: 14,
+                  bold: false,
+                  italic: false }},
+                  "legend": { "position": "right" }
+                };
 
     // The table id for the google fusion table that holds the data we want to query
     var tableId = '1fRrYNiGRZ8CiaihVL_jR37FRwbPdlYeJKuhJB92U';
@@ -79,23 +84,29 @@ agop.controller('ButtonController', ['$scope', function($scope) {
     queryObj.setQuery(query);
     queryObj.send(function(e) {
 
-        data = e.getDataTable();
+      data = e.getDataTable();
+      var data2 = data.Lf.sort(function(arr1, arr2){
+        return arr1.c[0].v - arr2.c[0].v;
+      });
+
+      console.log(data2);
 
         // Create a view for 2014 that is the first two columns of
         // the data, just the rows that have 2014 for the value.
-        var thisYear = $scope.year;
-                views[thisYear] = new google.visualization.DataView(data);
-                views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 8, value: thisYear}]));
-        views[thisYear].setColumns([0, 1, 2, 3, 4, 5, 6, 7]);
+        var thisYear = $scope.startYear;
+        var endYear = $scope.endYear;
+        views[thisYear + "," + endYear] = new google.visualization.DataView(data2);
+        views[thisYear + "," + endYear].setRows(views[thisYear + "," + endYear].getFilteredRows([{column: 0, minValue: thisYear, maxValue: endYear}]));
+        views[thisYear + "," + endYear].setColumns([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
+
+        console.log(views[thisYear + "," + endYear].toDataTable());
 
         // Draw the chart for 2014.
-        $scope.chart.draw(views[thisYear].toDataTable(), options);
-    });
+        $scope.chart.draw(views[thisYear + "," + endYear].toDataTable(), options);
 
-  // At the beginning of execution,
-  // the default start and end year is 2005 and 2015, respectively.
-  $scope.startYear = 2005;
-  $scope.endYear = 2014;
+        data = data2;
+      });
 
     // get()
     //    Get a new chart.
@@ -103,41 +114,61 @@ agop.controller('ButtonController', ['$scope', function($scope) {
 
     // If the view of data for the selected year hasn't been created
     // yet, create it.
-    if (views[thisYear] === undefined) {
+    if (views[thisYear + "," + endYear] === undefined) {
 
-        var thisYear = $scope.year;
-                views[thisYear] = new google.visualization.DataView(data);
-                views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 8, value: thisYear}]));
-                views[thisYear].setColumns([0, 1, 2, 3, 4, 5, 6, 7]);
 
-    }
+        // var data2 = data.Lf.sort(function(arr1, arr2){
+        //   return arr1.c[0].v - arr2.c[0].v;
+        // });
+var thisYear = $scope.startYear;
+var endYear = $scope.endYear;
+views[thisYear + "," + endYear] = new google.visualization.DataView(data);
+views[thisYear + "," + endYear].setRows(views[thisYear + "," + endYear].getFilteredRows([{column: 0, minValue: thisYear, maxValue: endYear}]));
+views[thisYear + "," + endYear].setColumns([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
+}
+
+console.log(views[thisYear + "," + endYear]);
+
     // Draw the chart for selected year.
-    $scope.chart.draw(views[thisYear].toDataTable(), options);
+    $scope.chart.draw(views[thisYear + "," + endYear].toDataTable(), options);
+    console.log("Drawing?");
 
-    };
+
+  };
 
   // minusStart():
   // decrements start year
   $scope.minusStart = function(){
     $scope.startYear = $scope.startYear - 1;
+    $scope.get();
   };
 
   // plusStart():
   // increments start year
   $scope.plusStart = function(){
-    $scope.startYear = $scope.startYear + 1;
+    if ($scope.startYear < $scope.endYear) {
+      $scope.startYear = $scope.startYear + 1;
+    }
+    $scope.get();
   };
 
   // minusEnd():
   // decrements end year
   $scope.minusEnd = function(){
-    $scope.endYear = $scope.endYear - 1;
+    if ($scope.endYear > $scope.startYear){
+      $scope.endYear = $scope.endYear - 1;
+    }
+    $scope.get();
+
   };
 
   // plusEnd():
   // increments end year
   $scope.plusEnd = function(){
     $scope.endYear = $scope.endYear + 1;
+    $scope.get();
+
   };
 
 }]);
